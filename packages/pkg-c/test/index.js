5e8e403561aafd56dda32547c09780cec917f2b2
Paul Berg <hello@paulrberg.com>
2019-03-28 20:57:07 +0000

    test(pkg-c): initial tests

---
+++ packages/pkg-c/test/index.js
@@ -0,0 +1,11 @@
+/* global describe, it */
+const { expect } = require("chai");
+
+require("../src");
+
+describe("computeConsoleTable", () => {
+    it("... should correctly import @prbpad/pkg-b and console.log the table", () => {
+        // importing the `src` folder suffices, there should be no error thrown there
+        expect(true).to.equal(true);
+    });
+});
